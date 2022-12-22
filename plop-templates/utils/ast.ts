8570abcbe835fe9ba62edb6e2f931748622f7c84
sunsiyuan <siyuan.sun1@baozun.com>
2022-12-22 21:51:37 +0800

    feat: 优化兼容性配置

--- plop-templates/utils/ast.ts
+++ plop-templates/utils/ast.ts
@@ -3,39 +3,58 @@ import type { ParseResult } from '@babel/parser';
 import * as type from '@babel/types';
 import generator from '@babel/generator';
 import { IRouteConfig } from 'plop-templates/actions.js';
+import chalk from 'chalk';
+
+const getRoutePathToAttribute = ({
+  name,
+  path: routePath,
+  component,
+}: IRouteConfig): type.ObjectExpression => {
+  return type.objectExpression([
+    type.objectProperty(type.identifier('name'), type.stringLiteral(name)),
+    type.objectProperty(type.identifier('path'), type.stringLiteral(routePath)),
+    type.objectProperty(
+      type.identifier('component'),
+      type.arrowFunctionExpression(
+        [],
+        type.callExpression(type.import(), [type.stringLiteral(component)])
+      )
+    ),
+  ]);
+};
+
+const isRoutesIdentifier = (path: any): boolean =>
+  type.isIdentifier(path.node) && path.node.name === 'routes';
+
+const isVariableDeclarationRoutes = (path: any): boolean =>
+  isRoutesIdentifier(path) && type.isVariableDeclarator(path.parent);
+
+const isValidPropertyRoutes = (path: any): boolean =>
+  isRoutesIdentifier(path) &&
+  type.isObjectProperty(path.parent) &&
+  !type.isIdentifier(path.parent.value);
+
+const dealRoutes = (path: any, insertNode: type.ObjectExpression) => {
+  if (isVariableDeclarationRoutes(path)) {
+    path.parent.init.elements.push(insertNode);
+  }
+  if (isValidPropertyRoutes(path)) {
+    path.parent.value.elements.push(insertNode);
+  }
+};
 
 // 这个函数会重写传入的routeAST对象内容
 export const editRouterAst = (
   routeAST: ParseResult<type.File>,
   routeConfig: IRouteConfig
 ): string => {
-  const { name, path: routePath, component } = routeConfig;
-  console.log(routeConfig);
   traverse.default(routeAST, {
     enter(path: any) {
-      if (type.isIdentifier(path.node) && path.node.name === 'routes') {
-        if (type.isObjectProperty(path.parent)) {
-          const insertNode = type.objectExpression([
-            type.objectProperty(
-              type.identifier('name'),
-              type.stringLiteral(name)
-            ),
-            type.objectProperty(
-              type.identifier('path'),
-              type.stringLiteral(routePath)
-            ),
-            type.objectProperty(
-              type.identifier('component'),
-              type.arrowFunctionExpression(
-                [],
-                type.callExpression(type.import(), [
-                  type.stringLiteral(component),
-                ])
-              )
-            ),
-          ]);
-          path.parent.value.elements.push(insertNode);
-        }
+      const insertNode = getRoutePathToAttribute(routeConfig);
+      try {
+        dealRoutes(path, insertNode);
+      } catch (e) {
+        console.log(chalk.red('something wrong!', e));
       }
     },
   });
