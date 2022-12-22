5ac133d11144f043d06b23c5a17bea12f3cc6d07
sunsiyuan <siyuan.sun1@baozun.com>
2022-12-22 18:39:33 +0800

    chore: 调整目录结构

--- types/config.d.ts
+++
@@ -1,9 +0,0 @@
-interface ITemplateConfig extends Record<string, any> {
-    baseDir: string;
-    componentBasePath: string;
-    viewBasePath: string;
-    sroreBasePath: string;
-    routesFilePath: string;
-}
-declare const conf: ITemplateConfig;
-export default conf;
